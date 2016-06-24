6509eafb673e612e8582a99edb30b0b9c8c929e3
kim7tin <tin@hs2t.com>
2016-06-24 18:58:01 +0700

    update test code

--- server/main.js
+++ server/main.js
@@ -3,6 +3,7 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
 	console.log((new Date()) + ' Received request for ' + request.url);
+	var body = "";
 	request.on('data', function(data) {
 		body += data;
 		console.log("Partial body: " + body);
